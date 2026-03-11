ab3259ac980021e8e28ab8b47130315c27eece4f
Aaron Lam <aaronlam1004@gmail.com>
2026-03-10 22:43:15 -0700

    [nvim] Adding LSP diagnostic key map

--- nvim/lua/self/keymap.lua
+++ nvim/lua/self/keymap.lua
@@ -1,3 +1,6 @@
 local telescope = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-p>', telescope.find_files, { desc = 'Telescope find_files' })
+vim.keymap.set('n', '<C-i>', function ()
+  vim.diagnostic.open_float(nil, { scope = "cursor" })
+end, { desc = 'Telescope find_files' })
